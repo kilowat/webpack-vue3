deac50e66102463952a3b282b31cdaa1fc511b38
Kilowat <kadantsev.sergey@gmail.com>
2022-03-25 11:38:57 +0300

    use setup tag

--- src/components/shared/SmallBasket.vue
+++ src/components/shared/SmallBasket.vue
@@ -1,19 +1,14 @@
-<template>
-  <div>basket items: {{ items }}</div>
-</template>
-<script>
+<script setup>
 import basket from '@/js/store/basket';
 import { onMounted } from '@vue/runtime-core';
 
-export default {
-  name: 'small-basket',
-  setup() {
-    onMounted(() => {
-      basket.init();
-    });
-    return {
-      items: basket.items,
-    }
-  },
-}
+onMounted(() => {
+  basket.init();
+});
+
+ const items = basket.items;
 </script>
+
+<template>
+  <div>basket items: {{ items }}</div>
+</template>
--- src/components/sliders/FullSlider.vue
+++ src/components/sliders/FullSlider.vue
@@ -1,3 +1,32 @@
+<script setup>
+
+import { onMounted, ref } from "@vue/runtime-core";
+import Swiper, { Navigation, Pagination } from "swiper";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+Swiper.use([Navigation, Pagination]);
+const slider = ref(null);
+const sliderInit = (rootEl) => {
+  const sw = new Swiper(rootEl, {
+    direction: "horizontal",
+    pagination: {
+      el: rootEl.querySelector(".swiper-pagination"),
+    },
+    navigation: {
+      nextEl: rootEl.querySelector(".swiper-button-next"),
+      prevEl: rootEl.querySelector(".swiper-button-prev"),
+    },
+  });
+};
+
+onMounted(() => {
+  sliderInit(slider.value);
+});
+
+</script>
+
 <template>
   <div class="swiper full-slider" ref="slider">
     <div class="swiper-wrapper">
@@ -12,40 +41,5 @@
     <div class="swiper-button-next"></div>
   </div>
 </template>
-<script>
-import { onMounted, ref } from "@vue/runtime-core";
-import Swiper, { Navigation, Pagination } from "swiper";
-import basket from '@/js/store/basket';
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
 
-export default {
-  name: "full-slider",
-  setup(props) {
-    Swiper.use([Navigation, Pagination]);
-    const slider = ref(null);
-    const sliderInit = (rootEl) => {
-      const sw = new Swiper(rootEl, {
-        direction: "horizontal",
-        pagination: {
-          el: rootEl.querySelector(".swiper-pagination"),
-        },
-        navigation: {
-          nextEl: rootEl.querySelector(".swiper-button-next"),
-          prevEl: rootEl.querySelector(".swiper-button-prev"),
-        },
-      });
-    };
-
-    onMounted(() => {
-      sliderInit(slider.value);
-    });
-
-    return {
-      slider,
-    };
-  },
-};
-</script>
 <style scoped lang="scss"></style>
